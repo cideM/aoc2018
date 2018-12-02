947dc227804d799e83578250cc7ab1524af1346a
Florian B <yuuki@protonmail.com>
2018-12-02 16:39:53 +0100

    formatting, comments

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import qualified Lib
+import qualified Day1
 import           Types
-import qualified Data.Text                     as Text
+import           Data.Map.Strict               as Map'
+import           Data.Text                     as Text
+import           Prelude                 hiding ( FilePath
+                                                , lookup
+                                                )
 import qualified Data.Text.IO                  as TextIO
 import           Options.Applicative
 import           Data.Semigroup                 ( (<>) )
@@ -19,19 +23,42 @@ stdInP = flag' StdIn (long "stdin" <> help "Read from stdin")
 inputP :: Parser Input
 inputP = fileInputP <|> stdInP
 
+dayP :: Parser DayNum
+dayP = strOption
+  (long "day" <> short 'd' <> metavar "DAYNUMBER" <> help "Advent of Code Day")
+
 opts :: Parser Options
-opts = Options <$> inputP
+opts = Options <$> inputP <*> dayP
+
+days :: Map DayNum DayProg
+days = Map'.fromList [("1", Day1.prog)]
+
+readDataFromFile :: FilePath -> IO Text
+readDataFromFile fp = TextIO.readFile $ Text.unpack fp
+
+readDataFromStdIn :: IO Text
+readDataFromStdIn = TextIO.getContents
+
+readData :: Options -> IO Text
+readData (Options (FileInput p) _) = readDataFromFile p
+readData (Options StdIn         _) = readDataFromStdIn
+
+getDayProg :: Options -> Maybe DayProg
+getDayProg (Options _ n) = lookup n days
 
 main :: IO ()
-main =
-  execParser program
-    >>= Lib.day1
-    >>= \case
-      (Left e) -> TextIO.putStrLn e
-      (Right r) -> TextIO.putStrLn . Text.pack $ show r
+main = do
+  opts <- execParser program
+  case getDayProg opts of
+    Nothing -> TextIO.putStrLn "No program found for that day!"
+    Just p  -> do
+      d <- readData opts
+      case _run p d of
+        (Left  e) -> TextIO.putStrLn e
+        (Right r) -> TextIO.putStrLn . Text.pack $ show r
  where
   program = info
     (helper <*> opts)
-    (fullDesc <> header "Advent of Code - Day 1" <> progDesc
-      "Adjust frequency abberrations"
+    (fullDesc <> header "Merry Wintersday :)" <> progDesc
+      "I miss Guild Wars 2 :("
     )
--- haskell/src/Types.hs
+++ haskell/src/Types.hs
@@ -3,12 +3,27 @@ module Types where
 import           Data.Text
 import           Prelude                 hiding ( FilePath )
 
+data DayProg = DayProg {
+    _name :: Text,
+    _run :: Text -> Either ErrMsg Solution
+                  }
+
+type Sum = Int
+type FirstRepeat = Maybe Int
+
+data Day1Result = Day1Result !Sum !FirstRepeat
+
 type ErrMsg = Text
+
+type Solution = Text
 
 type FilePath = Text
 
 data Input = FileInput !FilePath | StdIn
 
-newtype Options = Options {
-                       input :: Input
+type DayNum = Text
+
+data Options = Options {
+                       _input :: Input,
+                       _dayNum :: DayNum
                        }
