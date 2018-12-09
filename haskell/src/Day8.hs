1d78c5ff70ea39f0a4684fb87704a454d40e8ff7
Florian B <yuuki@protonmail.com>
2018-12-09 12:40:53 +0100

    haskell d8

--- haskell/src/Day8.hs
+++ haskell/src/Day8.hs
@@ -1,5 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
 
+-- This is *not* my solution. I did this in Rust first and then looked at other
+-- implementations. I was somewhat in awe when I saw the elegance of
+-- https://github.com/glguy/advent2018/blob/master/execs/Day08.hs
+-- So I went ahead and implemented it for myself, but referring to his code for
+-- directions.
 module Day8 where
 
 import qualified Control.Monad as Monad
@@ -14,9 +22,23 @@ type ChildCount = Int
 type MetaCount = Int
 
 data Node a =
-  Node [Node a]
-       [a] -- Children and meta data
-  deriving (Show)
+  Node [Node a] -- children
+       [a] -- meta data
+  deriving (Show, Functor, Foldable, Traversable)
+
+nodeValue :: Node Int -> Int
+nodeValue (Node xs ys)
+  | null xs = sum ys
+  | otherwise = sum $ fmap (maybe 0 nodeValue . getChildAtIndex) ys
+  where
+    getChildAtIndex i = indexOneBased i xs
+
+indexOneBased :: Int -> [a] -> Maybe a
+indexOneBased i xs
+  | i' >= 0 && i' < length xs = Just $ xs !! i'
+  | otherwise = Nothing
+  where
+    i' = i - 1
 
 nodeFromInts :: State [Int] (Node Int)
 nodeFromInts = do
@@ -35,7 +57,7 @@ run :: Text -> Either ErrMsg Text
 run t =
   let input = map (read . Text.unpack) $ Text.words t
       tree = evalState nodeFromInts input
-   in Right . Text.pack $ show tree
+   in Right . Text.pack $ show (sum tree) ++ " " ++ show (nodeValue tree)
 
 prog :: DayProg
 prog = DayProg "day8" run
