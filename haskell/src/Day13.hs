cfbdd4e2e0656faefa09b184f998f14448d0b5df
Florian B <yuuki@protonmail.com>
2019-01-11 22:43:23 +0100

    refactor haskell d16

--- haskell/src/Day13.hs
+++ haskell/src/Day13.hs
@@ -1,18 +1,19 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Day13 where
 
+import           Data.Foldable   (foldl')
+import           Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map, (!))
-import qualified Data.Maybe as Maybe
-import qualified Data.Text as Text
-import Data.Text (Text)
-import qualified Data.Text.IO as TextIO
-import Debug.Trace
-import Types
+import qualified Data.Maybe      as Maybe
+import           Data.Text       (Text)
+import qualified Data.Text       as Text
+import qualified Data.Text.IO    as TextIO
+import           Debug.Trace
+import           Types
 
 data Coords =
   Coords Int
@@ -37,7 +38,7 @@ data IntersectionDirection
   deriving (Show)
 
 data Cart = Cart
-  { velocityVector :: (Int, Int)
+  { velocityVector   :: (Int, Int)
   , nextIntersection :: IntersectionDirection
   } deriving (Show)
 
@@ -56,42 +57,42 @@ newVectorForCorner (Coords x y) cornerSymbol tracks
       else (1, -1) -- South west
 
 parseTracks :: Text -> (Map Coords Track, Map Coords Cart)
-parseTracks input = foldl f (Map.empty, Map.empty) lines'
+parseTracks input = foldl' f (Map.empty, Map.empty) lines'
   where
     lines' = zip [0 ..] . Prelude.lines $ Text.unpack input
     f acc (row, line) =
-      foldl
+      foldl'
         (\(tracks, carts) (col, char) ->
            let insert = Map.insert (Coords col row)
                makeCart velocity =
                  Cart {velocityVector = velocity, nextIntersection = Left'}
-           in case char of
-                '-' -> (insert (Straight Horizontal) tracks, carts)
-                '|' -> (insert (Straight Vertical) tracks, carts)
-                '+' -> (insert Intersection tracks, carts)
-                '/' ->
-                  ( insert
-                      (Corner (newVectorForCorner (Coords col row) char tracks))
-                      tracks
-                  , carts)
-                '\\' ->
-                  ( insert
-                      (Corner (newVectorForCorner (Coords col row) char tracks))
-                      tracks
-                  , carts)
-                '^' ->
-                  ( insert (Straight Vertical) tracks
-                  , insert (makeCart (0, -1)) carts)
-                'v' ->
-                  ( insert (Straight Vertical) tracks
-                  , insert (makeCart (0, 1)) carts)
-                '>' ->
-                  ( insert (Straight Horizontal) tracks
-                  , insert (makeCart (1, 0)) carts)
-                '<' ->
-                  ( insert (Straight Horizontal) tracks
-                  , insert (makeCart (-1, 0)) carts)
-                _ -> (tracks, carts))
+            in case char of
+                 '-' -> (insert (Straight Horizontal) tracks, carts)
+                 '|' -> (insert (Straight Vertical) tracks, carts)
+                 '+' -> (insert Intersection tracks, carts)
+                 '/' ->
+                   ( insert
+                       (Corner (newVectorForCorner (Coords col row) char tracks))
+                       tracks
+                   , carts)
+                 '\\' ->
+                   ( insert
+                       (Corner (newVectorForCorner (Coords col row) char tracks))
+                       tracks
+                   , carts)
+                 '^' ->
+                   ( insert (Straight Vertical) tracks
+                   , insert (makeCart (0, -1)) carts)
+                 'v' ->
+                   ( insert (Straight Vertical) tracks
+                   , insert (makeCart (0, 1)) carts)
+                 '>' ->
+                   ( insert (Straight Horizontal) tracks
+                   , insert (makeCart (1, 0)) carts)
+                 '<' ->
+                   ( insert (Straight Horizontal) tracks
+                   , insert (makeCart (-1, 0)) carts)
+                 _ -> (tracks, carts))
         acc
         (zip [0 ..] line)
 
@@ -106,7 +107,7 @@ advanceBy i carts tracks = go carts i
     go cs 0 = advanceCarts cs tracks
     go cs i' =
       let (carts', _) = advanceCarts cs tracks
-      in go carts' (i' - 1)
+       in go carts' (i' - 1)
 
 findCrash :: Map Coords Cart -> Map Coords Track -> Coords
 findCrash carts tracks = go carts []
@@ -114,7 +115,7 @@ findCrash carts tracks = go carts []
     go _ [x] = x
     go carts' _ =
       let (carts'', collisions) = advanceCarts carts' tracks
-      in go carts'' collisions
+       in go carts'' collisions
 
 findLastCart :: Map Coords Cart -> Map Coords Track -> Coords
 findLastCart carts tracks
@@ -124,16 +125,16 @@ findLastCart carts tracks
 newCartForIntersection :: Cart -> Cart
 newCartForIntersection Cart {nextIntersection = Left', velocityVector} =
   case velocityVector of
-    (1, 0) -> Cart (0, -1) Straight'
+    (1, 0)  -> Cart (0, -1) Straight'
     (0, -1) -> Cart (-1, 0) Straight'
     (-1, 0) -> Cart (0, 1) Straight'
-    (0, 1) -> Cart (1, 0) Straight'
+    (0, 1)  -> Cart (1, 0) Straight'
 newCartForIntersection Cart {nextIntersection = Right', velocityVector} =
   case velocityVector of
-    (1, 0) -> Cart (0, 1) Left'
+    (1, 0)  -> Cart (0, 1) Left'
     (0, -1) -> Cart (1, 0) Left'
     (-1, 0) -> Cart (0, -1) Left'
-    (0, 1) -> Cart (-1, 0) Left'
+    (0, 1)  -> Cart (-1, 0) Left'
 newCartForIntersection cart@Cart {nextIntersection = Straight'} =
   cart {nextIntersection = Right'}
 
@@ -151,22 +152,22 @@ advanceCarts carts tracks = Map.foldlWithKey f (carts, []) carts
                 let cart'@Cart {velocityVector = velocityVector'} =
                       newCartForIntersection c
                     (dx', dy') = velocityVector'
-                in (Coords (x + dx') (y + dy'), cart')
+                 in (Coords (x + dx') (y + dy'), cart')
               Corner (dx', dy') ->
                 let (dx'', dy'') = (dx + dx', dy + dy')
                     newCoords = Coords (x + dx'') (y + dy'')
-                in (newCoords, (c {velocityVector = (dx'', dy'')}))
+                 in (newCoords, (c {velocityVector = (dx'', dy'')}))
               _ -> (Coords (x + dx) (y + dy), c)
           withoutCurrent = Map.delete coords acc
           wasPartOfCollision = coords `elem` collisions
           hasCollision = Maybe.isJust (Map.lookup nextCoords withoutCurrent)
-      in if hasCollision
-           then (Map.delete nextCoords withoutCurrent, nextCoords : collisions)
+       in if hasCollision
+            then (Map.delete nextCoords withoutCurrent, nextCoords : collisions)
                -- somehow need to delete the deleted in its iteration
-           else if wasPartOfCollision
-                  then (withoutCurrent, collisions)
-                  else ( Map.insert nextCoords nextCar withoutCurrent
-                       , collisions)
+            else if wasPartOfCollision
+                   then (withoutCurrent, collisions)
+                   else ( Map.insert nextCoords nextCar withoutCurrent
+                        , collisions)
 
 run :: Text -> Either ErrMsg Text
 run t =
--- haskell/src/Day16.hs
+++ haskell/src/Day16.hs
@@ -1,20 +1,16 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Day16 where
 
 import           Control.Applicative   ((<|>))
 import           Data.Bits             ((.&.), (.|.))
-import           Data.Data             (Data)
-import qualified Data.Data             as Data
+import           Data.Foldable         (foldl')
 import           Data.IntMap.Strict    (IntMap, (!))
 import qualified Data.IntMap.Strict    as IntMap
-import qualified Data.List             as List
-import           Data.Set              (Set)
 import qualified Data.Set              as Set
 import           Data.Text             (Text)
 import qualified Data.Text             as Text
-import           Data.Typeable         (Typeable)
 import           Text.Parser.LookAhead (lookAhead)
 import           Text.Trifecta         (Parser, Result (Failure, Success))
 import qualified Text.Trifecta         as Tri
@@ -24,11 +20,11 @@ type Registers = IntMap Int
 
 newtype Register =
   Register Int
-  deriving (Show, Eq, Ord, Data, Typeable)
+  deriving (Show, Eq, Ord)
 
 newtype Immediate =
   Immediate Int
-  deriving (Show, Eq, Ord, Data)
+  deriving (Show, Eq, Ord)
 
 type OpCode = Int
 
@@ -39,56 +35,91 @@ data Instruction =
               Register
   deriving (Show, Eq, Ord)
 
-data Action
+data OpKind
   = ADDR Register
-         Register
          Register
   | ADDI Register
          Immediate
-         Register
   | MULR Register
-         Register
          Register
   | MULI Register
          Immediate
-         Register
   | BANR Register
-         Register
          Register
   | BANI Register
          Immediate
-         Register
   | BORR Register
-         Register
          Register
   | BORI Register
          Immediate
-         Register
   | SETR Register
-         Register
   | SETI Immediate
-         Register
   | GTIR Immediate
-         Register
          Register
   | GTRI Register
          Immediate
-         Register
   | GTRR Register
          Register
-         Register
-  | EQRI Immediate
-         Register
-         Register
-  | EQIR Register
+  | EQRI Register
          Immediate
+  | EQIR Immediate
          Register
   | EQRR Register
          Register
-         Register
-  deriving (Ord, Eq, Show, Data, Typeable)
+  deriving (Ord, Eq, Show)
 
 type Sample = (Registers, Instruction, Registers)
+
+type OpName = Text
+
+data Operation = Operation
+  { output :: Register
+  , kind   :: OpKind
+  , name   :: OpName
+  } deriving (Ord, Eq, Show)
+
+opFromIns :: Instruction -> OpName -> Maybe Operation
+opFromIns (Instruction _ inputA inputB output) opName = do
+  opKind <-
+    case opName of
+      "addr" -> Just $ ADDR (Register inputA) (Register inputB)
+      "addi" -> Just $ ADDI (Register inputA) (Immediate inputB)
+      "mulr" -> Just $ MULR (Register inputA) (Register inputB)
+      "muli" -> Just $ MULI (Register inputA) (Immediate inputB)
+      "banr" -> Just $ BANR (Register inputA) (Register inputB)
+      "bani" -> Just $ BANI (Register inputA) (Immediate inputB)
+      "borr" -> Just $ BORR (Register inputA) (Register inputB)
+      "bori" -> Just $ BORI (Register inputA) (Immediate inputB)
+      "gtir" -> Just $ GTIR (Immediate inputA) (Register inputB)
+      "gtri" -> Just $ GTRI (Register inputA) (Immediate inputB)
+      "gtrr" -> Just $ GTRR (Register inputA) (Register inputB)
+      "eqri" -> Just $ EQRI (Register inputA) (Immediate inputB)
+      "eqir" -> Just $ EQIR (Immediate inputA) (Register inputB)
+      "eqrr" -> Just $ EQRR (Register inputA) (Register inputB)
+      "setr" -> Just $ SETR (Register inputA)
+      "seti" -> Just $ SETI (Immediate inputA)
+      _      -> Nothing
+  return $ Operation output opKind opName
+
+opNames :: [Text]
+opNames =
+  [ "addr"
+  , "addi"
+  , "mulr"
+  , "muli"
+  , "banr"
+  , "bani"
+  , "borr"
+  , "bori"
+  , "setr"
+  , "seti"
+  , "gtir"
+  , "gtri"
+  , "gtrr"
+  , "eqri"
+  , "eqir"
+  , "eqrr"
+  ]
 
 registersP :: Parser Registers
 registersP = do
@@ -122,91 +153,48 @@ inputP = do
   p2input <- Tri.many $ instructionP <* Tri.spaces
   return (p1input, p2input)
 
-actionCreatorFromConstrString :: String -> (Instruction -> Action)
-actionCreatorFromConstrString consStr =
-  case consStr of
-    "ADDR" -> \(Instruction _ a b reg) -> ADDR (Register a) (Register b) reg
-    "ADDI" -> \(Instruction _ a b reg) -> ADDI (Register a) (Immediate b) reg
-    "MULR" -> \(Instruction _ a b reg) -> MULR (Register a) (Register b) reg
-    "MULI" -> \(Instruction _ a b reg) -> MULI (Register a) (Immediate b) reg
-    "BANR" -> \(Instruction _ a b reg) -> BANR (Register a) (Register b) reg
-    "BANI" -> \(Instruction _ a b reg) -> BANI (Register a) (Immediate b) reg
-    "BORR" -> \(Instruction _ a b reg) -> BORR (Register a) (Register b) reg
-    "BORI" -> \(Instruction _ a b reg) -> BORI (Register a) (Immediate b) reg
-    "SETR" -> \(Instruction _ a _ reg) -> SETR (Register a) reg
-    "SETI" -> \(Instruction _ a _ reg) -> SETI (Immediate a) reg
-    "GTIR" -> \(Instruction _ a b reg) -> GTIR (Immediate a) (Register b) reg
-    "GTRI" -> \(Instruction _ a b reg) -> GTRI (Register a) (Immediate b) reg
-    "GTRR" -> \(Instruction _ a b reg) -> GTRR (Register a) (Register b) reg
-    "EQRI" -> \(Instruction _ a b reg) -> EQRI (Immediate a) (Register b) reg
-    "EQIR" -> \(Instruction _ a b reg) -> EQIR (Register a) (Immediate b) reg
-    "EQRR" -> \(Instruction _ a b reg) -> EQRR (Register a) (Register b) reg
-
-makeActionsFromInstruction :: Instruction -> [Action]
-makeActionsFromInstruction (Instruction _ a b reg) =
-  [ ADDR (Register a) (Register b) reg
-  , ADDI (Register a) (Immediate b) reg
-  , MULR (Register a) (Register b) reg
-  , MULI (Register a) (Immediate b) reg
-  , BANR (Register a) (Register b) reg
-  , BANI (Register a) (Immediate b) reg
-  , BORR (Register a) (Register b) reg
-  , BORI (Register a) (Immediate b) reg
-  , SETR (Register a) reg
-  , SETI (Immediate a) reg
-  , GTIR (Immediate a) (Register b) reg
-  , GTRI (Register a) (Immediate b) reg
-  , GTRR (Register a) (Register b) reg
-  , EQRI (Immediate a) (Register b) reg
-  , EQIR (Register a) (Immediate b) reg
-  , EQRR (Register a) (Register b) reg
-  ]
-
-opCodeToConstrStr :: [Sample] -> Maybe (IntMap String)
-opCodeToConstrStr samples =
-  let opCodeMap = foldl buildMap IntMap.empty samples
-   in narrowMap opCodeMap IntMap.empty
+makeOpCodeMap :: [Sample] -> Maybe (IntMap OpName)
+makeOpCodeMap samples =
+  let (opCodeMap', _) = foldr f (IntMap.empty, Set.empty) samples
+   in if IntMap.size opCodeMap' /= length opNames
+        then Nothing
+        else Just opCodeMap'
   where
-    buildMap acc sample@(_, Instruction opCode _ _ _, _) =
-      let actions =
-            Set.fromList . map (Data.showConstr . Data.toConstr) $
-            runSample sample
-       in IntMap.insertWith Set.intersection opCode actions acc
-    narrowMap :: IntMap (Set String) -> IntMap String -> Maybe (IntMap String)
-    narrowMap mapIn mapOut
-      | IntMap.size mapIn == 0 = Just mapOut
-      | otherwise =
-        let solved = List.find ((==) 1 . length . snd) $ IntMap.assocs mapIn
-         in case solved of
-              Nothing -> Nothing
-              Just (k, v) ->
-                let constrStr = head $ Set.toList v
-                    mapOut' = IntMap.insert k constrStr mapOut
-                    mapIn' =
-                      IntMap.delete k (IntMap.map (Set.delete constrStr) mapIn)
-                 in narrowMap mapIn' mapOut'
+    f (before, ins@(Instruction opCode _ _ _), after) acc@(opCodeMap, known)
+      | IntMap.member opCode opCodeMap = acc
+      | otherwise = maybe acc checkOps $ traverse (opFromIns ins) opNames
+      where
+        checkOps ops =
+          let results = map (runOp before) ops
+              matchingBeforeAfter =
+                map snd . filter ((==) after . fst) $ zip results ops
+              unknown =
+                filter (not . flip Set.member known . name) matchingBeforeAfter
+           in if length unknown == 1
+                then let newOpName = name $ head unknown
+                      in ( IntMap.insert opCode newOpName opCodeMap
+                         , Set.insert newOpName known)
+                else acc
 
 p2 :: [Sample] -> [Instruction] -> Maybe Registers
 p2 samples instructions = do
-  opCodeMap <- opCodeToConstrStr samples
-  return $
-    foldl
-      (runProgram opCodeMap)
-      (IntMap.fromList [(0, 0), (1, 0), (2, 0), (3, 0)])
+  opCodeMap <- makeOpCodeMap samples
+  ops <-
+    traverse
+      (\ins@(Instruction opCode _ _ _) -> opFromIns ins $ opCodeMap ! opCode)
       instructions
-  where
-    runProgram opCodeMap acc ins@(Instruction opCode _ _ _) =
-      let actionCreator = actionCreatorFromConstrString $ opCodeMap ! opCode
-       in runAction acc (actionCreator ins)
+  return $ foldl' runOp (IntMap.fromList [(0, 0), (1, 0), (2, 0), (3, 0)]) ops
 
-runSample :: Sample -> [Action]
-runSample (before, instruction, after) =
-  let actions = makeActionsFromInstruction instruction
-      results = map (runAction before) actions
-   in map snd . filter ((==) after . fst) $ zip results actions
+runSample :: Sample -> Maybe [Operation]
+runSample (before, instruction, after) = do
+  ops <- traverse (opFromIns instruction) opNames
+  let results = map (runOp before) ops
+  return . map snd . filter ((==) after . fst) $ zip results ops
 
-p1 :: [Sample] -> Int
-p1 = length . filter (\x -> length x >= 3) . map runSample
+p1 :: [Sample] -> Maybe Int
+p1 samples = do
+  ops <- traverse runSample samples
+  return . length $ filter (flip (>=) 3 . length) ops
 
 parseInput :: Text -> Either ErrMsg ([Sample], [Instruction])
 parseInput input =
@@ -216,83 +204,74 @@ parseInput input =
   where
     str = Text.unpack input
 
-runAction :: Registers -> Action -> Registers
-runAction regs action =
-  case action of
-    ADDR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-       in insert regC (x + y)
-    ADDI (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-       in insert regC (x + y)
-    MULR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-       in insert regC (x * y)
-    MULI (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-       in insert regC (x * y)
-    BANR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-       in insert regC (x .&. y)
-    BANI (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-       in insert regC (x .&. y)
-    BORR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-       in insert regC (x .|. y)
-    BORI (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-       in insert regC (x .|. y)
-    SETR (Register regA) (Register regC) -> insert regC (regs ! regA)
-    SETI (Immediate x) (Register regC) -> insert regC x
-    GTIR (Immediate x) (Register regB) (Register regC) ->
-      let y = regs ! regB
-          v =
-            if x > y
-              then 1
-              else 0
-       in insert regC v
-    GTRI (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-          v =
-            if x > y
-              then 1
-              else 0
-       in insert regC v
-    GTRR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-          v =
-            if x > y
-              then 1
-              else 0
-       in insert regC v
-    EQRI (Immediate x) (Register regB) (Register regC) ->
-      let y = regs ! regB
-          v =
-            if x == y
-              then 1
-              else 0
-       in insert regC v
-    EQIR (Register regA) (Immediate y) (Register regC) ->
-      let x = regs ! regA
-          v =
-            if x == y
-              then 1
-              else 0
-       in insert regC v
-    EQRR (Register regA) (Register regB) (Register regC) ->
-      let x = regs ! regA
-          y = regs ! regB
-          v =
-            if x == y
-              then 1
-              else 0
-       in insert regC v
+runOp :: Registers -> Operation -> Registers
+runOp regs Operation {..} =
+  let Register outputReg = output
+      result =
+        case kind of
+          ADDR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in x + y
+          ADDI (Register regA) (Immediate y) ->
+            let x = regs ! regA
+             in x + y
+          MULR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in x * y
+          MULI (Register regA) (Immediate y) ->
+            let x = regs ! regA
+             in x * y
+          BANR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in x .&. y
+          BANI (Register regA) (Immediate y) ->
+            let x = regs ! regA
+             in x .&. y
+          BORR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in x .|. y
+          BORI (Register regA) (Immediate y) ->
+            let x = regs ! regA
+             in x .|. y
+          SETR (Register regA) -> regs ! regA
+          SETI (Immediate x) -> x
+          GTIR (Immediate x) (Register regB) ->
+            let y = regs ! regB
+             in if x > y
+                  then 1
+                  else 0
+          GTRI (Register regA) (Immediate y) ->
+            let x = regs ! regA
+             in if x > y
+                  then 1
+                  else 0
+          GTRR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in if x > y
+                  then 1
+                  else 0
+          EQRI (Register x) (Immediate regB) ->
+            let y = regs ! regB
+             in if x == y
+                  then 1
+                  else 0
+          EQIR (Immediate regA) (Register y) ->
+            let x = regs ! regA
+             in if x == y
+                  then 1
+                  else 0
+          EQRR (Register regA) (Register regB) ->
+            let x = regs ! regA
+                y = regs ! regB
+             in if x == y
+                  then 1
+                  else 0
+   in insert outputReg result
   where
     insert k v = IntMap.insert k v regs
 
