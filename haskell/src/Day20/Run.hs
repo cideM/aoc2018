8d733f266ef6e881f4b69033ef9b487f48bf6a67
Florian B <yuuki@protonmail.com>
2019-01-21 11:44:04 +0100

    gave up on d20

--- haskell/src/Day20/Run.hs
+++ haskell/src/Day20/Run.hs
@@ -5,112 +5,68 @@ module Day20.Run
   ( run
   ) where
 
-import           Data.Array      ((!))
-import qualified Data.Array      as Array
-import qualified Data.Foldable   as Foldable
-import           Data.Function   (on)
-import qualified Data.List       as List
-import qualified Data.List.Extra as Extra
-import           Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import           Data.Semigroup  (Max (..), Min (..))
-import           Data.Sequence   (Seq)
-import qualified Data.Sequence   as Seq
-import           Data.Set        (Set)
-import qualified Data.Set        as Set
-import           Data.Text       (Text)
-import qualified Data.Text       as Text
-import qualified Data.Text.IO    as TIO
-import           Day20.Parser    (parseInput)
-import           Day20.Types
-import           Debug.Trace
+import           Control.Monad.State
+import           Data.Map.Strict     (Map)
+import qualified Data.Map.Strict     as Map
+import qualified Data.Maybe          as Maybe
+import           Data.Sequence       (Seq)
+import qualified Data.Sequence       as Seq
+import           Data.Text           (Text)
+import qualified Data.Text           as Text
+import           Text.Printf
 import           Types
 
-flattenPaths :: [Path] -> [[Direction]]
-flattenPaths paths = go paths [[]]
-  where
-    go [] out = out
-    go (Step dir:rest) acc =
-      let acc' = (++) <$> acc <*> [[dir]]
-       in go rest acc'
-    go (Branches as:rest) acc =
-      let acc' = (++) <$> acc <*> concatMap flattenPaths as
-       in go rest acc'
+{-
+I copied this from
+https://www.reddit.com/r/adventofcode/comments/a7uk3f/2018_day_20_solutions/ec7cdtk
 
-go :: Point -> Direction -> Point
-go (Point (x, y)) N = Point (x, y - 1)
-go (Point (x, y)) E = Point (x + 1, y)
-go (Point (x, y)) W = Point (x - 1, y)
-go (Point (x, y)) S = Point (x, y + 1)
+I do understand how it works. I don't understand how I can come up with code
+like that from the start.
 
-makeWorld :: [[Direction]] -> World
-makeWorld =
-  foldr1 Map.union .
-  map (fst . List.foldl' f (Map.fromList [(origin, Room)], origin))
-  where
-    origin = Point (0, 0)
-    f (worldMap, loc) dir =
-      let firstStep = go loc dir
-          secondStep = go firstStep dir
-          map' = Map.insertWith (flip const) firstStep Door worldMap
-       in (Map.insertWith (flip const) secondStep Room map', secondStep)
+My solution was overcomplicated. It solved the test cases but ran seemingly
+forever on the actual data because it was about as inefficient as it gets and
+was just creating thunks. I'm so fucking frustrated right now. :( I hate
+Haskell but I also don't want to be one of the "stupid programmers" who can't
+handle it. And many solutions out there solve the problem with so few lines of
+code. I want to be able to do that too :(
+-}
+type Coords = (Int, Int)
 
-worldToGrid :: World -> Grid
-worldToGrid w =
-  let bounds =
-        foldMap (\(Point (x, y), _) -> Bounds (Min x) (Min y) (Max x) (Max y)) $
-        Map.assocs w
-      minPoint@(Point (x0, y0)) =
-        Point (getMin $ _minX bounds, getMin $ _minY bounds)
-      maxPoint@(Point (x1, y1)) =
-        Point (getMax $ _maxX bounds, getMax $ _maxY bounds)
-   in Array.array
-        (minPoint, maxPoint)
-        [ let p = Point (x, y)
-           in (p, Map.findWithDefault Wall p w)
-        | y <- [y0 .. y1]
-        , x <- [x0 .. x1]
-        ]
+type Doors = Map (Int, Int) [(Int, Int)]
 
-inputToWorld :: [[Path]] -> World
-inputToWorld = foldr1 Map.union . map (makeWorld . flattenPaths)
+parse :: [Coords] -> Coords -> String -> State Doors ()
+parse prev location@(x, y) (current:remaining) =
+  case current of
+    'N' -> connect location (x, y - 1) >>= \new -> parse prev new remaining
+    'E' -> connect location (x + 1, y) >>= \new -> parse prev new remaining
+    'W' -> connect location (x - 1, y) >>= \new -> parse prev new remaining
+    'S' -> connect location (x, y + 1) >>= \new -> parse prev new remaining
+    '(' -> parse (location : prev) location remaining
+    ')' -> parse (tail prev) location remaining
+    '|' -> parse prev (head prev) remaining
+    '$' -> pure ()
+    _   -> parse prev location remaining
 
-printGrid :: Grid -> IO ()
-printGrid grid =
-  let (Point (x, _), Point (x', _)) = Array.bounds grid
-      width = (x' - x) + 1
-   in mapM_ (TIO.putStrLn . Text.unwords) .
-      Extra.chunksOf width . map (Text.pack . show . snd) $
-      List.sortBy (compare `on` fst) (Array.assocs grid)
+connect :: Coords -> Coords -> State Doors Coords
+connect a b = do
+  modify $ Map.insertWith mappend a [b]
+  modify $ Map.insertWith mappend b [a]
+  return b
 
-getNeighbours :: Grid -> Point -> [Point]
-getNeighbours grid (Point (x, y)) =
-  let (Point (x0, y0), Point (x1, y1)) = Array.bounds grid
-      isInGrid (Point (x', y')) = x' >= x0 && x' <= x1 && y' >= y0 && y' <= y1
-   in filter
-        isInGrid
-        [Point (x, y + 1), Point (x, y - 1), Point (x + 1, y), Point (x - 1, y)]
-
-distances :: Grid -> Point -> Map Point Int
-distances g origin = go Map.empty $ Seq.singleton (origin, 0)
-  where
-    getNs = getNeighbours g
-    notWall p = (g ! p) /= Wall
-    go seen Seq.Empty = seen
-    go seen (p@(point, dist) Seq.:<| rest)
-      | Map.member point seen = go (traceShowId seen) rest
-      | otherwise = go seen' queue'
-      where
-        seen' = Map.insert point (traceShowId dist) seen
-        queue' =
-          rest Seq.><
-          (traceShowId . Seq.fromList . map (, dist + 1) . filter notWall $ getNs point)
+bfs :: Seq (Coords, Int) -> Map Coords Int -> Doors -> [Int]
+bfs Seq.Empty visited _ = Map.elems visited
+bfs ((coords, len) Seq.:<| queue) visited cnxs
+  | coords `Map.member` visited = bfs queue visited cnxs
+  | otherwise =
+    let next = Maybe.fromMaybe [] $ coords `Map.lookup` cnxs
+        next' = Seq.fromList $ (, len + 1) <$> next
+     in bfs (queue Seq.>< next') (Map.insert coords len visited) cnxs
 
 run :: Text -> Either ErrMsg Text
-run input = do
-  paths <- parseInput input
-  let world = inputToWorld paths
-      grid = worldToGrid world
-      dists = distances grid (Point (0,0))
-      maxDist = getMax . foldMap Max $ Map.elems dists
-  return . Text.pack $ "p1: " ++ show (maxDist `div` 2)
+run input =
+  let parsed = flip execState Map.empty $ parse [] (0, 0) (Text.unpack input)
+      paths = bfs (Seq.singleton ((0, 0), 0)) Map.empty parsed
+   in Right . Text.pack $
+      "p1: " ++
+      show (maximum paths) ++
+      " p2: " ++ show (length . filter (>= 1000) $ paths)
