716f91cbc8e6ee348e3209229120927f1835ea28
Florian B <yuuki@protonmail.com>
2019-01-25 10:01:51 +0100

    8 refactor

--- haskell/8.hs
+++ haskell/8.hs
@@ -1,57 +1,52 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE DeriveTraversable #-}
+#!/usr/bin/env stack
+{-
+    stack
+    script
+    --resolver lts-12.20
+    --package vector,mtl
+-}
+{-# LANGUAGE RecordWildCards #-}
 
--- This is *not* my solution. I did this in Rust first and then looked at other
--- implementations. I was somewhat in awe when I saw the elegance of
--- https://github.com/glguy/advent2018/blob/master/execs/Day08.hs
--- So I went ahead and implemented it for myself, but referring to his code for
--- directions.
-module Day8 (run) where
+module Day8 where
 
-import qualified Control.Monad as Monad
-import Control.Monad.State.Lazy
-import qualified Control.Monad.State.Lazy as State
-import qualified Data.Text as Text
-import Data.Text (Text)
-import Types
+import           Control.Arrow       ((&&&))
+import           Control.Monad       as Monad
+import           Control.Monad.State as State
+import qualified Data.Maybe          as Maybe
+import           Data.Semigroup      (Sum (..))
+import           Data.Vector         (Vector, (!?))
+import qualified Data.Vector         as Vector
 
-data Node a =
-  Node [Node a] -- children
-       [a] -- meta data
-  deriving (Show, Functor, Foldable, Traversable)
+data Node = Node
+  { children :: Vector Node
+  , metadata :: Vector Int
+  } deriving (Show)
 
-nodeValue :: Node Int -> Int
-nodeValue (Node xs ys)
-  | null xs = sum ys
-  | otherwise = sum $ fmap (maybe 0 nodeValue . getChildAtIndex) ys
-  where
-    getChildAtIndex i = indexOneBased i xs
+getNode :: State (Vector Int) Node
+getNode = do
+  x <- getOne
+  y <- getOne
+  Node <$> (Vector.fromList <$> Monad.replicateM x getNode) <*>
+    (Vector.fromList <$> Monad.replicateM y getOne)
 
-indexOneBased :: Int -> [a] -> Maybe a
-indexOneBased i xs
-  | i' >= 0 && i' < length xs = Just $ xs !! i'
-  | otherwise = Nothing
-  where
-    i' = i - 1
+getOne :: State (Vector Int) Int
+getOne = do
+  xs <- State.get
+  State.put $ Vector.tail xs
+  return $ Vector.head xs
 
-nodeFromInts :: State [Int] (Node Int)
-nodeFromInts = do
-  childCount <- getNext
-  metaCount <- getNext
-  Node <$> Monad.replicateM childCount nodeFromInts <*>
-    Monad.replicateM metaCount getNext
+metaSum :: Node -> Int
+metaSum Node {..} =
+  Vector.sum metadata + Vector.foldl' (\a b -> a + metaSum b) 0 children
 
-getNext :: State [a] a
-getNext = do
-  (x:rest) <- State.get
-  State.put rest
-  pure x
+nodeValue :: Node -> Int
+nodeValue Node {..} =
+  if Vector.null children
+    then Vector.sum metadata
+    else sum . map nodeValue . Maybe.catMaybes . Vector.toList $
+         Vector.map (\i -> children !? (i - 1)) metadata
 
-run :: Text -> Either ErrMsg Text
-run t =
-  let input = map (read . Text.unpack) $ Text.words t
-      tree = evalState nodeFromInts input
-   in Right . Text.pack $ show (sum tree) ++ " " ++ show (nodeValue tree)
-
+main :: IO ()
+main = do
+  input <- Vector.fromList . map read . words <$> getContents
+  print . (metaSum &&& nodeValue) $ State.evalState getNode input
