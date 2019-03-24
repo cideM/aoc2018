7340892fcdf85c6ef22fb5016864fe1c02e3a635
Florian B <yuuki@protonmail.com>
2019-03-24 22:18:24 +0100

    wip d23

--- haskell/23.hs
+++ haskell/23.hs
@@ -3,15 +3,66 @@
     stack
     script
     --resolver lts-13.6
-    --package transformers,mtl,megaparsec
+    --package transformers,mtl,megaparsec,text,vector
 -}
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.Void (Void)
-import Text.Megaparsec
-import Text.Megaparsec.Char
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
+import qualified Data.Vector                as V
+import           Data.Void                  (Void)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
-type Parser a = Parsec Void a
+type Parser = Parsec Void Text
 
+data Bot a = Bot
+  { x :: a
+  , y :: a
+  , z :: a
+  , r :: a
+  } deriving (Show, Eq)
 
+data Point a =
+  Point a
+        a
+        a
+  deriving (Show, Eq)
+
+botParser :: Parser (Bot Integer)
+botParser = do
+  _ <- string "pos=<"
+  _x <- signedDec <* char ','
+  _y <- signedDec <* char ','
+  _z <- signedDec
+  _ <- string ">, r="
+  _r <- signedDec
+  return $ Bot _x _y _z _r
+  where
+    signedDec = L.signed space L.decimal
+
+inputParser :: Parser (V.Vector (Bot Integer))
+inputParser = V.fromList <$> many (botParser <* space)
+
+distance :: Num a => Point a -> Point a -> a
+distance (Point x1 y1 z1) (Point x2 y2 z2) =
+  let x' = abs $ x2 - x1
+      y' = abs $ y2 - y1
+      z' = abs $ z2 - z1
+   in x' + y' + z'
+
+main :: IO ()
+main = do
+  parsed <- runParser inputParser "stdin" . T.pack <$> getContents
+  case parsed of
+    Left e -> print e
+    Right bots -> do
+      let strongest = V.maximumBy (\b1 b2 -> r b1 `compare` r b2) bots
+          inRangeOfStrongest =
+            V.filter (flip (<=) (r strongest) . distBots strongest) bots
+      print $ "Strongest: " ++ show strongest
+      print $ "In range of strongest: " ++ show (V.length inRangeOfStrongest)
+  where
+    distBots b1 b2 =
+      distance (Point (x b1) (y b1) (z b1)) (Point (x b2) (y b2) (z b2))
