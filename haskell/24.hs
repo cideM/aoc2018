46dd91d1f494ea816fdd682fe55fd52067e6d364
Florian B <yuuki@protonmail.com>
2019-04-24 22:52:20 +0200

    wip

--- haskell/24.hs
+++ haskell/24.hs
@@ -3,16 +3,23 @@
     stack
     script
     --resolver lts-13.16
-    --package text
+    --package text,megaparsec
 -}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
 
 type DamageType = Text
 
 data Attribute
   = WeakTo DamageType
   | ImmuneTo DamageType
+  deriving (Eq, Show)
 
 data Group a = Group
   { _name :: Text
@@ -20,10 +27,18 @@ data Group a = Group
   , _hitPointsPerUnit :: a
   , _initiative :: a
   , _damage :: a
-  , _effectivePower :: a,
+  , _effectivePower :: a
   , _damageType :: DamageType
   , _attributes :: [Attribute]
-  }
+  } deriving (Eq, Show)
+
+type Parser = Parsec Void Text
+
+groupParser :: Parser (Group Int)
+groupParser = do
+  groupName <- many (oneOf [spaceChar, letterChar]) <?> "group name"
+  _ <- symbolChar ";"
+  return $ Group (T.concat $ map T.singleton groupName) 0 0 0 0 0 "Foo" []
 
 main :: IO ()
 main = print "Hello"
