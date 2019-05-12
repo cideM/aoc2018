cae29d6916c25fee8112ab1fc82500b1d50aa30f
Florian B <yuuki@protonmail.com>
2019-05-12 18:36:13 +0200

    working input parses part 1

--- haskell/24.hs
+++ haskell/24.hs
@@ -5,16 +5,29 @@
     --resolver lts-13.16
     --package text,megaparsec
 -}
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Text (Text)
-import qualified Data.Text as T
-import Data.Void (Void)
-import Text.Megaparsec
-import Text.Megaparsec.Char
+import           Data.Functor               (($>))
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
+import           Data.Void                  (Void)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
-type DamageType = Text
+data Team a = Team
+  { _name   :: Text
+  , _groups :: [Group a]
+  } deriving (Show)
+
+data DamageType
+  = Fire
+  | Cold
+  | Radiation
+  | Slashing
+  | Bludgeoning
+  deriving (Eq, Show)
 
 data Attribute
   = WeakTo DamageType
@@ -22,23 +35,72 @@ data Attribute
   deriving (Eq, Show)
 
 data Group a = Group
-  { _name :: Text
-  , _unitCount :: a
+  { _unitCount        :: a
   , _hitPointsPerUnit :: a
-  , _initiative :: a
-  , _damage :: a
-  , _effectivePower :: a
-  , _damageType :: DamageType
-  , _attributes :: [Attribute]
+  , _initiative       :: a
+  , _damage           :: a
+  , _effectivePower   :: a
+  , _damageType       :: DamageType
+  , _attributes       :: Maybe [Attribute]
   } deriving (Eq, Show)
 
 type Parser = Parsec Void Text
 
+damageTypeParser :: Parser DamageType
+damageTypeParser =
+  space *>
+  ((string "fire" $> Fire) <|> (string "radiation" $> Radiation) <|>
+   (string "bludgeoning" $> Bludgeoning) <|>
+   (string "cold" $> Cold) <|>
+   (string "slashing" $> Slashing)) <*
+  space
+
+attributesParser :: Parser [Attribute]
+attributesParser = do
+  _ <- space
+  attributeTypeConstructor <-
+    (string "weak" $> WeakTo) <|> (string "immune" $> ImmuneTo)
+  _ <- string " to" <* space
+  damageTypes <- damageTypeParser `sepBy` char ',' <* space
+  return $ fmap attributeTypeConstructor damageTypes
+
+attributesParser' :: Parser [Attribute]
+attributesParser' = concat <$> attributesParser `sepBy` char ';'
+
+parens = between (char '(') (char ')')
+
 groupParser :: Parser (Group Int)
 groupParser = do
-  groupName <- many (oneOf [spaceChar, letterChar]) <?> "group name"
-  _ <- symbolChar ";"
-  return $ Group (T.concat $ map T.singleton groupName) 0 0 0 0 0 "Foo" []
+  unitCount <- L.decimal <* space
+  _ <- string "units each with "
+  hitPointsPerUnit <- L.decimal
+  _ <- string " hit points" <* space
+  attributes <- optional $ parens attributesParser' <* space
+  attack <- string "with an attack that does " *> L.decimal <* space
+  damageType <- damageTypeParser <* "damage at initiative" <* space
+  initiative <- L.decimal <* space
+  return $
+    Group
+      { _unitCount = unitCount
+      , _hitPointsPerUnit = hitPointsPerUnit
+      , _attributes = attributes
+      , _damage = attack
+      , _effectivePower = unitCount * attack
+      , _damageType = damageType
+      , _initiative = initiative
+      }
+
+teamParser :: Parser (Team Int)
+teamParser = do
+  name <-
+    T.pack <$> (manyTill (choice [letterChar, spaceChar]) (char ':') <* space)
+  groups <- many groupParser
+  return $ Team name groups
+
+inputParser = do
+  many teamParser <* space
 
 main :: IO ()
-main = print "Hello"
+main = do
+  parsed <- runParser inputParser "stdin" . T.pack <$> getContents
+  print parsed
