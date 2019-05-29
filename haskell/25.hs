27bd6efa8ad860ef7aca3c35a0aa837378a86f9b
Florian B <yuuki@protonmail.com>
2019-05-29 08:59:42 +0200

    DONE

--- haskell/25.hs
+++ haskell/25.hs
@@ -3,73 +3,65 @@
     stack
     script
     --resolver lts-13.16
-    --package unordered-containers,hashable
+    --package pretty-simple,megaparsec,text,fgl
 -}
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Applicative               (empty)
 import           Data.Foldable
-import           Data.Hashable
-import           Data.HashSet               (HashSet)
-import           Data.Text                  (Text)
-import qualified Data.HashSet               as HashSet
-import qualified Data.List                  as List
-import           Data.Void                  (Void)
-import           GHC.Generics               (Generic)
-import           Text.Megaparsec            hiding (State)
+import qualified Data.Graph.Inductive.Graph        as Graph
+import           Data.Graph.Inductive.PatriciaTree (UGr)
+import           Data.Graph.Inductive.Query        as Query
+import qualified Data.List                         as List
+import           Data.Text                         (Text)
+import qualified Data.Text                         as T
+import           Data.Void                         (Void)
+import           Debug.Pretty.Simple
+import           Text.Megaparsec                   hiding (State)
 import           Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+import qualified Text.Megaparsec.Char.Lexer        as L
+import           Text.Pretty.Simple                (pPrint)
 
 data Point =
   Point Int
         Int
         Int
         Int
-  deriving (Show, Eq, Generic)
-
-instance Hashable Point
-
-isConnected :: HashSet Point -> Point -> Bool
-isConnected set p = any (\p' -> distance p' p <= 3) set
+  deriving (Show, Eq)
 
 distance :: Point -> Point -> Int
 distance (Point a b c d) (Point a' b' c' d') =
-  abs (a - a') + abs (b - b') + abs (c + c') + abs (d + d')
+  abs (a - a') + abs (b - b') + abs (c - c') + abs (d - d')
 
 origin :: Point
 origin = Point 0 0 0 0
 
-constellation :: [Point] -> [HashSet Point]
-constellation [] = []
-constellation ps =
-  let byDistFromOrigin = List.sortOn (distance origin) ps
-   in go [] byDistFromOrigin
-  where
-    go out [] = out
-    go out (p:ps) =
-      let newConstellation =
-            foldr
-              (\p acc ->
-                 if isConnected acc p
-                   then HashSet.insert p acc
-                   else acc)
-              (HashSet.singleton p)
-              ps
-          remaining =
-            HashSet.toList $
-            HashSet.difference (HashSet.fromList ps) newConstellation
-       in go (newConstellation : out) remaining
-
 main :: IO ()
-main = print "Hello"
+main = do
+  parsed <- runParser inputParser "stdin" . T.pack <$> getContents
+  case parsed of
+    Left e -> pPrint e
+    Right points ->
+      let withIds = zip [1 ..] points
+          edges =
+            [ (x, y)
+            | (x, p1) <- withIds
+            , (y, p2) <- withIds
+            , distance p1 p2 <= 3
+            ]
+       in pPrint . Query.noComponents $
+          (Graph.mkUGraph [1 .. length points] edges :: UGr)
 
 -- ############# PARSING
 type Parser = Parsec Void Text
 
 inputParser :: Parser [Point]
-inputParser = many pointParser <* space
+inputParser =
+  many $
+  (Point <$> signedInteger <* comma <*> signedInteger <* comma <*> signedInteger <*
+   comma <*>
+   signedInteger) <*
+  space
   where
-    pointParser = signedInteger <* space `sepBy` char ','
-    lexeme = L.lexeme space
-    integer = lexeme L.decimal
-    signedInteger = L.signed space integer
+    comma = L.symbol space ","
+    signedInteger = L.signed space L.decimal
